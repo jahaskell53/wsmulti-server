9edbb392fe8d0b554ca075f90b0f0eb1413c9263
Jakobi Haskell <jakobi_haskell@brown.edeu>
2022-02-22 23:04:39 -0500

    working version

---
+++ server.js
@@ -0,0 +1,19 @@
+const io = require('socket.io')(3000, {
+    // options parameter for fun, to allow cors
+    cors: {
+        // pass in whatever domain client connects to (vrwikitest.com)
+        origin: ['http://127.0.0.1:5500']
+    }
+
+})
+
+io.on('connection', (socket) => {
+    // give all clients an id
+    // console.log(socket.id);
+    // have to be connected before sending event
+    socket.on('controller', (data) => {
+        // goal to broadcast this data to all clients except the one that sent it
+        socket.broadcast.emit('send-controller', data)
+        console.log(data);
+    })
+});
