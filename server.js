def24c02455b8d7d44af969aa2ccc890c8c4bd10
Jakobi Haskell <jakobi_haskell@brown.edeu>
2022-03-05 23:40:27 -0500

    adds all caps to consts

--- server.js
+++ server.js
@@ -1,36 +1,39 @@
-const { readFileSync } = require('fs');
-const { createServer } = require('https');
-const { Server } = require('socket.io');
+const { readFileSync } = require("fs");
+const { createServer } = require("https");
+const { Server } = require("socket.io");
+
+// TOOD: replace with immutable data types in TS
+const PORT = 3000;
+const KEY_PATH = "/opt/bitnami/letsencrypt/certificates/www.vrwikitest.com.key";
+const CERT_PATH = "/opt/bitnami/letsencrypt/certificates/www.vrwikitest.com.crt";
+const SERVER_DOMAIN = "https://www.vrwikitest.com";
 
 const options = {
-    // options parameter for creating server port, to allow cors
-    cors: {
-        // pass in whatever domain client connects to (vrwikitest.com)
-        origin: ['https://www.vrwikitest.com']
-    }
-
+  // options parameter for creating server port, to allow cors
+  cors: {
+    // pass in whatever domain client connects to (vrwikitest.com)
+    origin: [SERVER_DOMAIN],
+  },
 };
 
 // read the key and server stored in the aws lightsail, for using a secure ws connection
-const keyPath = '/opt/bitnami/letsencrypt/certificates/www.vrwikitest.com.key'
-const certPath = "/opt/bitnami/letsencrypt/certificates/www.vrwikitest.com.crt"
 const httpServer = createServer({
-    key: readFileSync(keyPath),
-    cert: readFileSync(certPath)
-})
+  key: readFileSync(KEY_PATH),
+  cert: readFileSync(CERT_PATH),
+});
 // create a new server and store it as an io object
-const io = new Server(httpServer, options)
-    // have to be connected before sending event, restricting our second event to only get handled when connection is active
-    // connection gets passed socket obj automatically as argument
-io.on('connection', (socket) => {
-    // when client connects, broadcast that to all other clients 
-    socket.broadcast.emit('user-joined', socket.id)
-    console.log("New user id:", socket.id)
-    // when the server receives a message on controller channel,
-    socket.on('update-to', (userObj) => {
-        // broadcasts this data to all clients except the one that sent it
-        socket.broadcast.emit('update-send', userObj)
-    })
+const io = new Server(httpServer, options);
+// have to be connected before sending event, restricting our second event to only get handled when connection is active
+// connection gets passed socket obj automatically as argument
+io.on("connection", (socket) => {
+  // when client connects, broadcast that to all other clients
+  socket.broadcast.emit("user-joined", socket.id);
+  console.log("New user id:", socket.id);
+  // when the server receives a message on controller channel,
+  socket.on("update-to", (userObj) => {
+    // broadcasts this data to all clients except the one that sent it
+    socket.broadcast.emit("update-send", userObj);
+  });
 });
 
-httpServer.listen(3000);
+httpServer.listen(PORT);
