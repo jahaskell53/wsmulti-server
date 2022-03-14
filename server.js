1afcfd4de9d3dba3dc924678fad01186a0414d1b
Jakobi Haskell <jakobi_haskell@brown.edu>
2022-03-13 20:24:51 -0400

    removes todo and adds better doc

--- server.js
+++ server.js
@@ -1,40 +1,39 @@
-"use strict";
-exports.__esModule = true;
-var fs_1 = require("fs");
-var https_1 = require("https");
-var socket_io_1 = require("socket.io");
-// TOOD: replace with immutable data types in TS
-var PORT = 3000;
-var KEY_PATH = "/opt/bitnami/letsencrypt/certificates/www.vrwikitest.com.key";
-var CERT_PATH = "/opt/bitnami/letsencrypt/certificates/www.vrwikitest.com.crt";
-var SERVER_DOMAIN = "https://www.vrwikitest.com";
-var options = {
-    // options parameter for creating server port, to allow cors
-    cors: {
-        // pass in whatever domain client connects to (vrwikitest.com)
-        origin: [SERVER_DOMAIN]
-    }
+import { readFileSync } from "fs";
+import { createServer } from "https";
+import { Server } from "socket.io";
+
+// replace these constants with your own config
+const SERVER_PORT = 3000; 
+const KEY_PATH = "/opt/bitnami/letsencrypt/certificates/www.vrwikitest.com.key";
+const CERT_PATH =
+  "/opt/bitnami/letsencrypt/certificates/www.vrwikitest.com.crt";
+const SERVER_DOMAIN = "https://www.vrwikitest.com";
+
+const options = {
+  // allows cors (Cross-Origin-Resource Sharing) so client can connect to server
+  cors: {
+    // pass in whatever domain server is on
+    origin: [SERVER_DOMAIN],
+  },
 };
-// read the key and server stored in the aws lightsail, for using a secure ws connection
-var httpServer = (0, https_1.createServer)({
-    key: (0, fs_1.readFileSync)(KEY_PATH),
-    cert: (0, fs_1.readFileSync)(CERT_PATH)
+
+// read the key and server stored in the aws lightsail in order to use secure connection
+const httpServer = createServer({
+  key: readFileSync(KEY_PATH),
+  cert: readFileSync(CERT_PATH),
 });
-// create a new server and store it as an io object
-var io = new socket_io_1.Server(httpServer, options);
-// have to be connected before sending event, restricting our second event to only get handled when connection is active
-// connection gets passed socket obj automatically as argument
-io.on("connection", function (socket) {
-    // when client connects, broadcast that to all other clients
-    socket.broadcast.emit("user-joined", socket.id);
-    console.log("New user id:", socket.id);
-    // when the server receives a message on controller channel,
-    socket.on("update-to", function (userObj) {
-        // broadcasts this data to all clients except the one that sent it
-        socket.broadcast.emit("update-send", userObj, socket.id);
-    });
-    socket.on("disconnect", function () {
-        socket.broadcast.emit("disconnect-send", socket.id);
-    });
+const io = new Server(httpServer, options);
+
+io.on("connection", (socket) => {
+  // when client connects, broadcast that to all other clients
+  socket.broadcast.emit("user-joined", socket.id);
+  console.log("New user id:", socket.id);
+  socket.on("update-to", (userObj) => {
+    socket.broadcast.emit("update-send", userObj, socket.id);
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("disconnect-send", socket.id);
+  });
 });
-httpServer.listen(PORT);
+
+httpServer.listen(SERVER_PORT);
